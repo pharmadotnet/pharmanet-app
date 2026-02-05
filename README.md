6e5421ce6c4e47d038c15c94c53ed1a312d34992
bilalsul <workubilal@gmail.com>
2026-02-05 22:16:29 +0300

    init pharmanet flutter app

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pharma Net Mobile And Web Support
---
+++ android/app/src/main/kotlin/com/example/pharmanet_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pharmanet_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
